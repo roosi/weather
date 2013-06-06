54ccf839f8b73a3c709d6b07b355be54242e9633
roosi <roosikrantsi@gmail.com>
2013-06-06 20:46:41 +0300

    Added: Hide forecast button when not available

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,18 @@
 var utils = {
 	getDefaultLocale: function() {
 		return (navigator.userLanguage||navigator.browserLanguage||navigator.language).substr(0,2);
+	},
+	
+	checkMobile: function() {
+		var sw=window.innerWidth
+			|| document.documentElement.clientWidth
+			|| document.body.clientWidth;
+
+		var sh=window.innerHeight
+			|| document.documentElement.clientHeight
+			|| document.body.clientHeight;
+		
+		var breakpoint = 640;
+		return (sw > breakpoint) ? false : true;
 	}
 }
